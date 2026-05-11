2eb4f7990b77f3cd29ec6d3e03e404af9666ee0d
LuisFzg <luisfzuluaga2004@hotmail.com>
2026-05-11 01:13:08 -0500

    Deployment fix 9

--- server/api/[...path].js
+++ server/api/[...path].js
@@ -1,3 +1,31 @@
-const appModule = require('../dist/app');
+let app;
 
-module.exports = appModule.default || appModule.app;
+function applyCors(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    req.headers['access-control-request-headers'] || 'Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    req.headers['access-control-request-method'] || 'GET,POST,PATCH,OPTIONS'
+  );
+  res.setHeader('Vary', 'Access-Control-Request-Headers, Access-Control-Request-Method');
+}
+
+module.exports = (req, res) => {
+  applyCors(req, res);
+
+  if (req.method === 'OPTIONS') {
+    res.statusCode = 204;
+    res.end();
+    return;
+  }
+
+  if (!app) {
+    const appModule = require('../dist/app');
+    app = appModule.default || appModule.app;
+  }
+
+  return app(req, res);
+};
--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,46 @@
-const appModule = require('../dist/app');
+let app;
 
-module.exports = appModule.default || appModule.app;
+function applyCors(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    req.headers['access-control-request-headers'] || 'Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    req.headers['access-control-request-method'] || 'GET,POST,PATCH,OPTIONS'
+  );
+  res.setHeader('Vary', 'Access-Control-Request-Headers, Access-Control-Request-Method');
+}
+
+function restoreExpressUrl(req) {
+  const parsedUrl = new URL(req.url || '/', `https://${req.headers.host || 'localhost'}`);
+  const path = parsedUrl.searchParams.get('path');
+
+  if (!path) {
+    return;
+  }
+
+  parsedUrl.searchParams.delete('path');
+  const search = parsedUrl.searchParams.toString();
+  req.url = `/api/${path}${search ? `?${search}` : ''}`;
+}
+
+module.exports = (req, res) => {
+  applyCors(req, res);
+
+  if (req.method === 'OPTIONS') {
+    res.statusCode = 204;
+    res.end();
+    return;
+  }
+
+  restoreExpressUrl(req);
+
+  if (!app) {
+    const appModule = require('../dist/app');
+    app = appModule.default || appModule.app;
+  }
+
+  return app(req, res);
+};
